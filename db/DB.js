adcd93e6e2f218fbd9d9003edbcf2979dfcd23f6
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2024-05-07 20:42:13 +0530

    DB Done with with needed variables commented

--- db/DB.js
+++ db/DB.js
@@ -1,5 +1,5 @@
-export var users = [];
-export var customers = [];
-export var products = [];
-export var orders = [];
-export var orderDetails = [];
+export var users = []; //username, email address, password 
+export var customers = []; //customerId, customerName, customerAddress, customerPhone
+export var products = []; //productId, productName, productType, productQty, productPrice
+export var orders = []; //orderId, customerId, customerName, orderDate, orderTotalPrice
+export var orderDetails = []; //orderId, orderDetailid, productId, productName, productType, productQty, productPrice, productTotalPrice  
